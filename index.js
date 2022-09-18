f8f21b6ba81885d0425e86749b86e3dc0827a32b
96group <citzebedee@gmail.com>
2022-09-18 15:54:33 +0800

    array to object

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/production', async(request, response) => {
     try{
         const control = await Control.find()
 
-        response.json(control)
+        response.json(control[0])
         
     }catch(e){
         response.json({message: e})
